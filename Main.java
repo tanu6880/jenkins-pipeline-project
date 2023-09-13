80581b2c1b81acd48e44936d178a34814b3b0901
Tanu <tanusaini6880@gmail.com>
2023-09-13 14:17:08 +0530

    missing semicolon

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ public class Main{
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hii Tanu Saini")
+		System.out.println("Hii Tanu Saini");
 		for(int i=0;i<10;i++)
 		{
 			for(int j=i;j<10;j++)
 			{
-				System.out.println("* ")
+				System.out.println("* ");
 			}
 
 			System.out.println();
